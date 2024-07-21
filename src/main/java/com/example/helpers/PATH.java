f4c089b59007241d7ffde7188535c7b8354b6369
Abhishek Shrestha <meabhi224@gmail.com>
2024-07-21 13:05:27 +0545

    added CourseDataController

--- src/main/java/com/example/helpers/PATH.java
+++ src/main/java/com/example/helpers/PATH.java
@@ -4,7 +4,8 @@ import java.nio.file.Paths;
 
 public enum PATH {
   QUIZ(Paths.get("src", "main", "java", "com", "example", "database", "quiz.json").toString()),
-  USER(Paths.get("src", "main", "java", "com", "example", "database", "user.json").toString());
+  USER(Paths.get("src", "main", "java", "com", "example", "database", "user.json").toString()),
+  COURSE(Paths.get("src", "main", "java", "com", "example", "database", "course.json").toString());
 
   private final String filePath;
 
