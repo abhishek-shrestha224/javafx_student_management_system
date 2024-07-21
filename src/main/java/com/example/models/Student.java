5f7a972bfbb31fdffdd7425427efb99a1aa00c95
Abhishek Shrestha <meabhi224@gmail.com>
2024-07-21 21:06:11 +0545

    ...

--- src/main/java/com/example/models/Student.java
+++ src/main/java/com/example/models/Student.java
@@ -5,21 +5,11 @@ import java.time.LocalDate;
 import com.example.helpers.Utils;
 
 public class Student extends User {
-  private final LocalDate dob;
-  private final Course course;
 
   public Student(String firstName, String lastName, String email, String password, Gender gender,
       Course course, LocalDate dob) {
     super(firstName, lastName, email, Utils.generateId(Role.STUDENT.toString()), password, gender, Role.STUDENT);
-    this.course = course;
-    this.dob = dob;
+
   }
 
-  public LocalDate getDob() {
-    return dob;
-  }
-
-  public Course getCourse() {
-    return course;
-  }
 }
